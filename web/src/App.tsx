f8fe5d11236fe9dcbdb5d4558cc58ec116c46a16
guilhermepdc <guilherme.carvalho@edu.pucrs.br>
2020-08-11 12:29:40 -0300

    create styles global.css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './assets/styles/global.css';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Bátima!</h1>
+      <h1>Hello Bátima! 1:00:00 v1</h1>
     </div>
   );
 }
